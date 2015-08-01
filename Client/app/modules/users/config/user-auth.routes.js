e8ce352b35f94643b8a6af19ea839af9335d09b9
Steve Boisvert <leseulsteve@gmail.com>
2015-08-01 15:03:21 -0400

    Supression controlleurs login et signup

--- Client/app/modules/users/config/user-auth.routes.js
+++ Client/app/modules/users/config/user-auth.routes.js
@@ -7,14 +7,23 @@ angular.module('users').config(
 
       .state('userForm', {
       url: '/utilisateur/ajout',
-      templateUrl: 'modules/users/views/users.form.html',
-      controller: 'UsersAjoutController'
+      templateUrl: 'modules/users/views/users.form.html'
     })
 
     .state('login', {
       url: '/login',
-      templateUrl: 'modules/users/views/users.login.html',
-      controller: 'LoginController'
-    });
+      templateUrl: 'modules/users/views/users.login.html'
+    })
 
+    .state('confirm_email', {
+      url: '/confirm_email/:tokenId',
+      templateUrl: 'modules/users/views/users.email-confirm.html',
+      controller: function ($scope, UserAuth) {
+        UserAuth.confirmEmail().then(function () {
+          $scope.emailConfirmed = true;
+        }).catch(function () {
+          $scope.emailConfirmed = false;
+        });
+      }
+    });
   });
