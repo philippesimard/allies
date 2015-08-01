4fb309110dfd33bf93d4866f99e25725ef7e03a7
Steve Boisvert <leseulsteve@gmail.com>
2015-08-01 15:02:46 -0400

    Ajout function user.toString()

--- Client/app/modules/users/services/users.factory.js
+++ Client/app/modules/users/services/users.factory.js
@@ -3,7 +3,16 @@
 angular.module('users').factory('User',
   function (Ressource) {
 
-    return new Ressource({
+    var User = new Ressource({
       api: 'user'
     });
+
+    User.pre('return', function (user) {
+      user.toString = function () {
+        return user.firstname + ' ' + user.lastname;
+      };
+      return user;
+    });
+
+    return User;
   });
