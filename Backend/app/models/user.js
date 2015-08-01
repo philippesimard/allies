8abce0e21547d621af696ae63479b0997e7d606a
Steve Boisvert <leseulsteve@gmail.com>
2015-08-01 14:59:55 -0400

    ajout champs + changement du username lors du changement d'email

--- Backend/app/models/user.js
+++ Backend/app/models/user.js
@@ -3,10 +3,38 @@
 var mongoose = require('mongoose'),
 	extend = require('mongoose-schema-extend'),
 	UserAuth = require('express-user-auth'),
-	Schema = mongoose.Schema;
+	Schema = mongoose.Schema,
+	_ = require('lodash-node');
 
 var UserSchema = UserAuth.getSecureUserSchema().extend({
-	test: String
+	lastname: {
+		type: String,
+		trim: true,
+		required: true
+	},
+	firstname: {
+		type: String,
+		trim: true,
+		required: true
+	},
+	dateNaissance: {
+		type: Date,
+		required: true
+	},
+	pseudo: {
+		type: String,
+		trim: true,
+		required: true
+	}
+});
+
+
+UserAuth.getSecureUserSchema().pre('save', function(next) {
+
+	if (this.isModified('email')) {
+		this.username = this.email;
+	}
+	next();
 });
 
 module.exports = mongoose.model('User', UserSchema);
