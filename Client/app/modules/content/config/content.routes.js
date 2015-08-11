413ceb2c3e9207b3793cc014dcb4f35280a00c98
Philipe Simard <philipesimard@icloud.com>
2015-08-10 21:45:09 -0400

    Ressources
    Ajout d’une catégorie ressources pour films,livres,documents,etc.

---
+++ Client/app/modules/content/config/content.routes.js
@@ -0,0 +1,45 @@
+'use strict';
+
+angular.module('content').config(
+  function ($stateProvider, $urlRouterProvider) {
+
+    $urlRouterProvider.otherwise('/');
+
+    $stateProvider
+
+      .state('film', {
+      url: '/contenus/film',
+      templateUrl: 'modules/content/views/film.html',
+      controller: 'ContentController'
+    })
+
+    .state('livre', {
+      url: '/contenus/livre',
+      templateUrl: 'modules/content/views/livre.html',
+      controller: 'ContentController'
+    })
+
+    .state('bd', {
+      url: '/contenus/bd',
+      templateUrl: 'modules/content/views/bd.html',
+      controller: 'ContentController'
+    })
+
+    .state('document', {
+      url: '/contenus/document',
+      templateUrl: 'modules/content/views/document.html',
+      controller: 'ContentController'
+    })
+
+    .state('roman', {
+      url: '/contenus/roman',
+      templateUrl: 'modules/content/views/roman.html',
+      controller: 'ContentController'
+    })
+
+    .state('autre', {
+      url: '/contenus/autre',
+      templateUrl: 'modules/content/views/autre.html',
+      controller: 'ContentController'
+    });
+  });
---
+++ Client/app/modules/content/controllers/content.controller.js
@@ -0,0 +1,6 @@
+'use strict';
+
+angular.module('content').controller('ContentController',
+  function () {
+
+  });
