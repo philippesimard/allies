d07542f21452dbaa71bc1b954e05422a98932403
Steve Boisvert <leseulsteve@gmail.com>
2015-09-03 16:17:44 -0400

    Corrections

--- Client/app/js/config.js
+++ Client/app/js/config.js
@@ -4,7 +4,7 @@
 var ApplicationConfiguration = (function () {
   // Init module configuration options
   var applicationModuleName = 'angularjsapp';
-  var applicationModuleVendorDependencies = ['ngTouch', 'ngSanitize', 'ui.router', 'ngResource', 'ngMessages', 'elasticui', 'uuid', 'angular.userAuth'];
+  var applicationModuleVendorDependencies = ['ngTouch', 'ngSanitize', 'ui.router', 'ngResource', 'ngMessages', 'elasticui', 'uuid', 'leseulsteve.angular-user-auth'];
 
   // Add a new vertical module
   var registerModule = function (moduleName, dependencies) {
