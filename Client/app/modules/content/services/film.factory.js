c5ddd28937e3330e6a80f12f6860a90cc30d256b
Philipe Simard <philipesimard@icloud.com>
2015-09-08 22:49:08 -0400

    Corrections  content

--- Client/app/modules/content/services/film.factory.js
+++ Client/app/modules/content/services/film.factory.js
@@ -5,14 +5,18 @@ angular.module('users').factory('Film',
 
     var Film = new Schema('film');
 
-    /* Creer nouveau film
+    /*
     Film.create({
-      nom: 'nom',
-      synopsis: 'synopsis'
-      niveau: '343fkf3049244jf'
+      name: 'La belle saison',
+      synopsis: 'Delphine, fille de paysans, monte à Paris pour s’émanciper du carcan familial et gagner son indépendance financière. Carole est parisienne. En couple avec Manuel, elle vit activement les débuts du féminisme. Lorsque Delphine et Carole se rencontrent, leur histoire d’amour fait basculer leurs vies.',
+      realisateur: 'Catherine Corsini',
+      niveau: '55eb5d604c85b5f824ad9518',
+      annee: '2015',
+      dureeMins: '105',
+      img: 'film/saison.jpg',
+
     });
-  
-    */
+*/
 
     Film.post('find', function (next) {
       this.imgUrl = 'images/' + this.img;
