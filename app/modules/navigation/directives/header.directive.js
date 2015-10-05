fd7aa8af39e7c9f11a9cab774552d5af19d33869
leseulsteve <leseulsteve@gmail.com>
2015-10-05 19:36:13 -0400

    Menu dynamique

--- app/modules/navigation/directives/header.directive.js
+++ app/modules/navigation/directives/header.directive.js
@@ -1,12 +1,33 @@
 'use strict';
 
 angular.module('core').directive('header',
-  function (UserAuth, $window, $state, $rootScope) {
+  function (UserAuth, $window, $state, $rootScope, Parcours, MediaSection) {
 
     return {
 
       templateUrl: 'modules/navigation/views/header.html',
       link: function (scope) {
+
+        Parcours.find().then(function (parcours) {
+          scope.allParcours = parcours;
+        });
+
+        MediaSection.find().then(function (mediaSections) {
+          scope.mediaSections = mediaSections;
+        });
+
+
+        scope.goToParcours = function (parcours) {
+          $state.go('parcours-page', {
+            parcoursName: parcours.shortName
+          });
+        };
+
+        scope.goToMediaSection = function (mediaSection) {
+          $state.go('media-section', {
+            mediaSectionName: section.shortName
+          });
+        };
 
         scope.isConnected = $rootScope.currentUser.isAuthentified();
 
