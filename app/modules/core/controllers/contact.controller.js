a120375f2cebbe30c880666d29677b25f4621f2c
philippesimard <philipesimard@icloud.com>
2015-10-13 14:21:52 -0400

    Formulaire de contact

--- app/modules/core/controllers/contact.controller.js
+++ app/modules/core/controllers/contact.controller.js
@@ -1,6 +1,20 @@
 'use strict';
 
 angular.module('core').controller('ContactController',
-  function () {
+  function ($rootScope, $scope, Mailer, MaterializeService) {
 
+    $scope.send = function (contactForm) {
+
+      if (contactForm.$valid) {
+
+        $scope.message.from = $rootScope.currentUser.firstname + ' ' + $rootScope.currentUser.lastname + ' <' + $rootScope.currentUser.email + '>';
+        $scope.message.to = 'info';
+
+        Mailer.send($scope.message).then(function () {
+          $scope.message = {};
+          contactForm.$setPristine();
+          MaterializeService.toast('Le message à été envoyé!');
+        });
+      }
+    };
   });
