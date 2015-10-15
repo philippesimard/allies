608dd4371f3f367a8e8b8ea25f4a09905b5c183a
philippesimard <philipesimard@icloud.com>
2015-10-15 18:37:22 -0400

    ANGULAR BREADCRUMB

--- app/js/config.js
+++ app/js/config.js
@@ -4,7 +4,7 @@
 var ApplicationConfiguration = (function () {
   // Init module configuration options
   var applicationModuleName = 'angularjsapp';
-  var applicationModuleVendorDependencies = ['ngTouch', 'ngSanitize', 'ui.router', 'ngMessages', 'uuid', 'elasticui', 'leseulsteve.angular-user-auth', 'leseulsteve.angular-mongoose', 'angular.img', '720kb.tooltips', 'ui.materialize', 'ngMorph'];
+  var applicationModuleVendorDependencies = ['ngTouch', 'ngSanitize', 'ui.router', 'ngMessages', 'uuid', 'elasticui', 'leseulsteve.angular-user-auth', 'leseulsteve.angular-mongoose', 'angular.img', '720kb.tooltips', 'ui.materialize', 'ngMorph', 'ncy-angular-breadcrumb'];
 
   // Add a new vertical module
   var registerModule = function (moduleName, dependencies) {
