7a2b19938787e58ab485391550df966ad25e0a03
leseulsteve <leseulsteve@gmail.com>
2015-10-17 11:58:25 -0400

    Affichage des contenus sauvegardés par l'utilisateur

--- app/modules/users/controllers/contenus.controller.js
+++ app/modules/users/controllers/contenus.controller.js
@@ -1,6 +1,39 @@
 'use strict';
 
 angular.module('users').controller('ContenusController',
-  function () {
+  function ($rootScope, $scope) {
+
+    $scope.savedThings = [{
+      title: 'Medias',
+      color: 'blue',
+      icon: 'movie',
+      things: $rootScope.currentUser.favorites.media || [],
+      show: function (things) {
+        $scope.medias = things;
+      }
+    }, {
+      title: 'Pistes',
+      color: 'pink',
+      icon: 'link',
+      things: $rootScope.currentUser.favorites.piste || [],
+      show: function (things) {
+        $scope.pistes = things;
+      }
+    }];
+
+    $scope.show = function (savedThing) {
+      $scope.medias = undefined;
+      $scope.pistes = undefined;
+      _.forEach($scope.savedThings, function (thing) {
+        if (thing === savedThing) {
+          thing.active = true;
+        } else {
+          thing.active = false;
+        }
+      });
+      savedThing.show(savedThing.things);
+    };
+
+    $scope.show($scope.savedThings[0]);
 
   });
