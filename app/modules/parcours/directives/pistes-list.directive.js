7447386b62fff38496edc213e3bfd82d7148f7dc
leseulsteve <leseulsteve@gmail.com>
2015-10-17 11:57:42 -0400

    Possibilité d'afficher la liste des parcours par leur ids

--- app/modules/parcours/directives/pistes-list.directive.js
+++ app/modules/parcours/directives/pistes-list.directive.js
@@ -1,12 +1,31 @@
 'use strict';
 
 angular.module('parcours').directive('pisteList',
-  function ($state) {
+  function (Piste, $q, $state) {
 
     return {
       restrict: 'E',
+      scope: {
+        pistes: '=',
+        pistesIds: '='
+      },
       templateUrl: 'modules/parcours/views/pistes.list.html',
-      link: function (scope) {
+      link: function (scope, element, attrs) {
+
+        scope.noHeader = attrs.noHeader;
+
+        if (scope.pistesIds) {
+          var promises = [],
+            pistes = [];
+          _.forEach(scope.pistesIds, function (pisteId) {
+            promises.push(Piste.findById(pisteId).then(function (piste) {
+              pistes.push(piste);
+            }));
+          });
+          $q.when(promises).then(function () {
+            scope.pistes = pistes;
+          });
+        }
 
         scope.toToPiste = function (piste) {
           $state.go('piste-page', {
