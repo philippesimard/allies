0e574dc749f79c3b9aae796280b090c7ac425edc
philippesimard <philipesimard@icloud.com>
2015-10-18 15:42:45 -0400

    Blog Wordpress JSON
    https://github.com/philippesimard/allies/issues/18

--- app/modules/core/controllers/recherche.controller.js
+++ app/modules/core/controllers/recherche.controller.js
@@ -1,6 +1,16 @@
 'use strict';
 
 angular.module('core').controller('RechercheController',
-  function () {
+  function ($scope, $http) {
+
+    var url = 'http://acommealliees.ca/cdn/data/wp-json/posts';
+
+    $http.jsonp(url)
+      .success(function (data) {
+        console.log(data.found);
+        $scope.content = data;
+      });
 
   });
+
+// Http.get --> Articles récent: http://acommealliees.ca/cdn/data/wp-json/posts
