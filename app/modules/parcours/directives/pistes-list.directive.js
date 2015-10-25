7f334df925374e32d84999ad85962d2e252a944e
leseulsteve <leseulsteve@gmail.com>
2015-10-25 17:28:22 -0400

    On peut maintenant supprimer des piste de nos favoris! yeah!

--- app/modules/parcours/directives/pistes-list.directive.js
+++ app/modules/parcours/directives/pistes-list.directive.js
@@ -1,28 +1,25 @@
 'use strict';
 
 angular.module('parcours').directive('pisteList',
-  function (Piste, $q, $state) {
+  function ($rootScope, MaterializeService, Piste, $q, $state) {
 
     return {
       restrict: 'E',
       scope: {
         pistes: '=',
-        pistesIds: '='
+        pistesIds: '=',
+        showRemoveIcon: '@',
+        noHeader: '@'
       },
       templateUrl: 'modules/parcours/views/pistes.list.html',
-      link: function (scope, element, attrs) {
-
-        scope.noHeader = attrs.noHeader;
+      link: function (scope) {
 
         if (scope.pistesIds) {
-          var promises = [],
-            pistes = [];
+          var promises = [];
           _.forEach(scope.pistesIds, function (pisteId) {
-            promises.push(Piste.findById(pisteId).then(function (piste) {
-              pistes.push(piste);
-            }));
+            promises.push(Piste.findById(pisteId));
           });
-          $q.when(promises).then(function () {
+          $q.when(promises).then(function (pistes) {
             scope.pistes = pistes;
           });
         }
@@ -30,6 +27,13 @@ angular.module('parcours').directive('pisteList',
         scope.toToPiste = function (piste) {
           $state.go('piste-page', {
             pisteName: piste.shortName
+          });
+        };
+
+        scope.removeFromFavorite = function (piste) {
+          $rootScope.currentUser.removeFavorite('piste', piste).then(function (message) {
+            _.pull(scope.pistes, piste);
+            MaterializeService.toast('<i class="material-icons red-text">favorite</i>&nbsp;&nbsp;' + message, 3000);
           });
         };
       }
