d2429ce0ed7ca856075c08e987527bcca29c840e
philippesimard <philipesimard@icloud.com>
2015-10-29 14:30:19 -0400

    Interface

--- app/modules/core/controllers/recherche.controller.js
+++ app/modules/core/controllers/recherche.controller.js
@@ -1,15 +1,7 @@
 'use strict';
 
 angular.module('core').controller('RechercheController',
-  function ($scope, $http) {
-
-    var url = 'http://acommealliees.ca/cdn/data/wp-json/posts';
-
-    $http.jsonp(url)
-      .success(function (data) {
-        console.log(data.found);
-        $scope.content = data;
-      });
+  function () {
 
   });
 
