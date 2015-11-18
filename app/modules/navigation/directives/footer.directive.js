32bcb69c4c00b34691d59b0bbb88d96b22d07d87
philippesimard <philipesimard@icloud.com>
2015-11-18 11:35:36 -0500

    Déplacement footer et share dans des directives

---
+++ app/modules/navigation/directives/footer.directive.js
@@ -0,0 +1,11 @@
+'use strict';
+
+angular.module('core').directive('ft',
+  function () {
+
+    return {
+      restrict: 'E',
+      templateUrl: 'modules/navigation/views/footer.html'
+    };
+
+  });
