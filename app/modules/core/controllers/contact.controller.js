7c640d96334328e8c26c502ea359a9587b3191df
leseulsteve <leseulsteve@gmail.com>
2015-11-22 11:28:51 -0500

    #32 Standardisation du formulaire de contact

--- app/modules/core/controllers/contact.controller.js
+++ app/modules/core/controllers/contact.controller.js
@@ -1,6 +1,33 @@
 'use strict';
 
-angular.module('core').controller('ContactController',
-  function () {
+angular.module('users').controller('ContactController',
+  function ($rootScope, $scope, Mailer, MaterializeService) {
 
+    function initContactForm() {
+      if ($rootScope.currentUser.isAuthentified()) {
+        $scope.message = {
+          name: $rootScope.currentUser.firstname + ' ' + $rootScope.currentUser.lastname,
+          email: $rootScope.currentUser.email
+        };
+      }
+    }
+
+    initContactForm();
+
+    $scope.send = function (contactForm) {
+
+      if (contactForm.$valid) {
+
+        $scope.message.from = $scope.message.name + ' <' + $scope.message.email + '>';
+        $scope.message.to = 'info';
+
+        Mailer.send($scope.message).then(function () {
+          $scope.message = {};
+          $scope.mail = {};
+          contactForm.$setPristine();
+          MaterializeService.toast('Le message à été envoyé. Merci!', 3000);
+          initContactForm();
+        });
+      }
+    };
   });
