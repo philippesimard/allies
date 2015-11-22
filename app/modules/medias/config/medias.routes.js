bc1667503fbf4e28e454662add68bc033d1cae8d
leseulsteve <leseulsteve@gmail.com>
2015-11-22 18:21:00 -0500

    Bug affichage des fiches de média

--- app/modules/medias/config/medias.routes.js
+++ app/modules/medias/config/medias.routes.js
@@ -1,19 +1,19 @@
 'use strict';
 
 angular.module('medias').config(
-  function ($stateProvider) {
+  function($stateProvider) {
 
     $stateProvider
 
-      .state('ressources', {
+    .state('ressources', {
       url: '/mediagraphie',
       templateUrl: 'modules/medias/views/media-sections.page.html',
       resolve: {
-        sections: function (MediaSection) {
+        sections: function(MediaSection) {
           return MediaSection.find();
         }
       },
-      controller: function ($scope, sections) {
+      controller: function($scope, sections) {
         $scope.mediaSections = sections;
       }
     })
@@ -21,14 +21,14 @@ angular.module('medias').config(
     .state('media-section', {
       url: '/mediagraphie/:mediaSectionName',
       resolve: {
-        section: function (MediaSection, $stateParams) {
+        section: function(MediaSection, $stateParams) {
           return MediaSection.findOne({
             shortName: $stateParams.mediaSectionName
           });
         }
       },
       templateUrl: 'modules/medias/views/media.section.html',
-      controller: function ($scope, section) {
+      controller: function($scope, section) {
         $scope.section = section;
       }
     })
@@ -36,21 +36,27 @@ angular.module('medias').config(
     .state('media-fiche', {
       url: '/mediagraphie/media/:mediaId',
       resolve: {
-        media: function (Media, $stateParams) {
+        media: function(Media, $stateParams) {
           return Media.findOne({
             _id: $stateParams.mediaId
           });
         }
       },
-      templateProvider: function ($http, media, MediaSection) {
-        return MediaSection.getFicheTemplateUrl(media.sectionId).then(function (templateUrl) {
-          return $http.get(templateUrl).then(function (response) {
-            return response.data;
-          });
+      templateProvider: function($http, media, MediaSection, ENV, $q, $templateCache) {
+        return MediaSection.getFicheTemplateUrl(media.sectionId).then(function(templateUrl) {
+          if (ENV === 'prod') {
+            var deffered = $q.defer();
+            deffered.resolve($templateCache.get(templateUrl));
+            return deffered.promise;
+          } else {
+            return $http.get(templateUrl).then(function(response) {
+              return response.data;
+            });
+          }
         });
 
       },
-      controller: function ($scope, media) {
+      controller: function($scope, media) {
         $scope.media = media;
       }
     });
