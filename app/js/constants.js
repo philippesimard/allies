d9ce962ddc5124684c525bc01480e8791f40d380
leseulsteve <leseulsteve@gmail.com>
2016-01-10 12:47:54 -0500

    no message

--- app/js/constants.js
+++ app/js/constants.js
@@ -2,6 +2,10 @@
 
 angular.module('config', [])
 
-.constant('BACKEND', 'http://localhost:9001')
+.constant('BACKEND', 'http://vps54578.vps.ovh.ca')
+
+.constant('CLIENT_URL', 'http://www.acommealliees.ca')
+
+.constant('ENV', 'prod')
 
 ;
