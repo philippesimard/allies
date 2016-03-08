1b68cf84dfe07477664e246c74615c9369ce6dd2
philippesimard <philipesimard@icloud.com>
2016-03-08 18:24:46 -0500

    Médias à la une

--- app/modules/parcours/directives/parcours-preview.directive.js
+++ app/modules/parcours/directives/parcours-preview.directive.js
@@ -1,21 +1,4 @@
 'use strict';
 
 angular.module('parcours').directive('parcoursPreview',
-  function ($state) {
-    return {
-      restrict: 'E',
-      scope: {
-        parcours: '='
-      },
-      templateUrl: 'modules/parcours/views/parcours.preview.html',
-      link: function (scope, element) {
-        angular.element(element.find('div')[0]).css('background-image', 'url(' + scope.parcours.img + ')');
-
-        scope.toToParcours = function (parcours) {
-          $state.go('parcours-page', {
-            parcoursName: parcours.shortName
-          });
-        };
-      }
-    };
-  });
+  function () {});
